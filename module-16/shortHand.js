97e7c15739bf93510452533cf5586780d8973d8b
wdminarul <wdminarul100@gmail.com>
2024-07-26 21:29:10 +0600

    short-hand-math

---
+++ module-16/shortHand.js
@@ -0,0 +1,23 @@
+ var price1 = 20;
+
+//  shorthant addition 
+price1 = price1 + 10;
+price1 += 10; //same think more short 
+
+//  shorthant substract 
+price1 = price1 -5;
+price1 -= 5; //same think more short 
+
+//  shorthant multiply 
+price1 = price1 * 2;
+price1 *= 2; //same think more short 
+
+//  shorthant division 
+price1 = price1 / 2;
+price1 /= 2; //same think more short 
+
+var age = 22 ;
+age++; //++ mean adding 1 exiting value 
+console.log(age)
+
+age--; //-- mean adding 1 loss value 
