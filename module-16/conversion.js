a03dfb0f0a9ec35c4cf3a0ea1d37406cfba184eb
wdminarul <wdminarul100@gmail.com>
2024-07-26 21:45:11 +0600

    conversion

---
+++ module-16/conversion.js
@@ -0,0 +1,13 @@
+// convert string to number 
+var price1 = "100";
+var price2 = "500" ;
+var gpa = '4.25';
+
+gpa = parseFloat(gpa);
+
+price1 = parseInt(price1); // conver a string as number 
+price2 = parseInt(price2)
+
+var totalPrice = price1 + price2;
+console.log(totalPrice)
+console.log(gpa);
