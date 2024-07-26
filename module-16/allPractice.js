337f94196d83ec96fc8a7a92da2e24e1a93f0cf1
wdminarul <wdminarul100@gmail.com>
2024-07-26 23:47:00 +0600

    all-practice

---
+++ module-16/allPractice.js
@@ -0,0 +1,30 @@
+// addision math some vasiable 
+var momGiveMoney = 1000;
+var boysName = "Aden jones";
+var orange = 175;
+var apple = 245;
+var mango =340 ;
+var totalBuy = orange + apple + mango;
+var residualMoney = momGiveMoney - totalBuy ;
+var expense = ' ' + 'taka expense' ;
+
+console.log(boysName , residualMoney ,expense )
+
+// avarage mark from school subjects 
+
+var math = 92 ;
+var bangla = 74;
+var english = 85 ;
+var socialScience = 89 ;
+var physics = 84;
+var chemistry = 94 ;
+var subjects = 6 ;
+
+var totalMark = math + bangla + english + socialScience + physics + chemistry;
+var avarageMark = totalMark / subjects;
+avarageMark = avarageMark.toFixed(2)
+
+console.log(parseFloat(avarageMark))
+
+
+
