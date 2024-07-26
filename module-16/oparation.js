7b45f11b574b12cf05f882e6b9b4e176285d8b34
wdminarul <wdminarul100@gmail.com>
2024-07-26 21:14:13 +0600

    simple-math-oparation

--- module-16/oparation.js
+++ module-16/oparation.js
@@ -1,4 +1,28 @@
+// addition 
+
 var eggPrice = 48;
 var chickenPrice = 200;
-var productsname = "egg, chicken"
-console.log(chickenPrice)
+var totalPrice = eggPrice+chickenPrice;
+console.log(totalPrice)
+
+// subtraction
+
+var poketMoney = 200 ;
+var rikshawVara = 60 ;
+var totalAce = poketMoney - rikshawVara;
+
+console.log(totalAce);
+
+// multiply 
+var oragePrice = 50;
+var orageQuantity = 24 ;
+var totalOrage = oragePrice * orageQuantity;
+
+console.log(totalOrage)
+
+// division 
+var winMoney = 5000;
+var playerQuentity = 10;
+var perPlayer = winMoney / playerQuentity ;
+
+console.log(perPlayer);
