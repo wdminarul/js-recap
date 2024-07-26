d3a6a75c3fe231e7608a6852ce7c7c784044d34b
wdminarul <wdminarul100@gmail.com>
2024-07-26 22:14:53 +0600

    js-typeOf-and-vagfol

---
+++ module-16/remainder.js
@@ -0,0 +1,18 @@
+var oragePrice = 22;
+var person = 5 ;
+
+var vagFol = oragePrice % person; // using for know vagfoll
+
+console.log(vagFol);
+
+var number1= 0.1;
+var number2 = 0.2;
+
+var totalNumber = number1 + number2;
+console.log(totalNumber) 
+
+// using don't show multiple decimal number 
+
+totalNumber = totalNumber.toFixed(1);
+
+console.log(parseFloat(totalNumber)) // using string to number covert 
---
+++ module-16/type.js
@@ -0,0 +1,9 @@
+var price = 100;
+var fullName = "shimul";
+var male = true;
+var single;
+
+console.log(typeof price) // show the types of veriable 
+console.log(typeof fullName) // show the types of veriable 
+console.log(typeof male) // show the types of veriable 
+console.log(typeof single) // show the types of veriable 
