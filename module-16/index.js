b56100bdad34ed07757faefd65a47970decafc6c
wdminarul <wdminarul100@gmail.com>
2024-07-26 20:42:40 +0600

    first commit

---
+++ module-16/index.js
@@ -0,0 +1,16 @@
+// java script is a interprated high lebel language 
+// Basic types of java script = string, numeric, Boolean, undifiend, null 
+// ::::::variable declare system 5 thinks::::
+// 1. var, const , let, = type of veriable 
+// 2. name of veriable 
+        // 1. camel case = totalPrice
+        // 2. snake case = total_price
+        // 3.postal case = TotalPrice
+        // (but maximum time we are use camel case for variable name )
+// 3. using separation "=" sign 
+// 4. variable value 
+// 5.using colse for semi-clone ;
+
+var total = 100 ; //numeric type variable 
+var name = "shiimul" ; //string type variable
+var youAreHuman = true ; //boolen type veriable 
